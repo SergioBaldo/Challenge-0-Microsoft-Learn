5549d287ac23f123071436332584a9742fdace65
Sergio <s.baldojunior@gmail.com>
2023-08-13 10:22:30 -0300

    Change Parameters

--- src/model/train.py
+++ src/model/train.py
@@ -63,7 +63,7 @@ def parse_args():
     # add arguments
     parser.add_argument("--training_data", dest="training_data", type=str)
     parser.add_argument(
-        "--reg_rate", dest="reg_rate", type=float, default=0.01
+        "--reg_rate", dest="reg_rate", type=float, default=0.02
     )
 
     # parse args
